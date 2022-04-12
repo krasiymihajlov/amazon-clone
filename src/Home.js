a34fe2daafc896426f634c6349a9b3fabfc982ad
K.Mihailov <kmihailov@pointserv.com>
2022-04-12 23:41:08 +0300

    Start App and add Header Component

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css';
+
+function Home() {
+    return(
+        <div className='home'>
+            <p>Fuck IT</p>
+        </div>
+    )
+}
+
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAPAdK_VxhW6FLMqtLaT6jfxeWrveQudHE",
+    authDomain: "fir-d9d80.firebaseapp.com",
+    projectId: "fir-d9d80",
+    storageBucket: "fir-d9d80.appspot.com",
+    messagingSenderId: "81081873721",
+    appId: "1:81081873721:web:1a598e7f4d168ef67624e8",
+    measurementId: "G-F5DVQVHG8N"
+  };
